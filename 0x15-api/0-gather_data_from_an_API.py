5465f8645afe9d5555f086156dd96ea95568c83c
Niamask <niamaskay@gmail.com>
2023-12-06 00:51:04 -0800

    update 0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
+"""module to fetch URL"""
 
-'''
-    Gathers data from an API.
-'''
-
-import requests as req
-from sys import argv as arg
-
-
-def getData():
-    '''
-        Gets data from an API.
-    '''
-    ListOfUsers = req.get('https://jsonplaceholder.typicode.com/users').json()
-    ListOfTodos = req.get('https://jsonplaceholder.typicode.com/todos').json()
-    TotalTasks = 0
-    TotalCompletedTasks = 0
-    TaskDescription = []
-    for user in ListOfUsers:
-        if user.get('id') == int(arg[1]):
-            Employee = user.get('name')
-            break
-    for todo in ListOfTodos:
-        if todo.get('userId') == int(arg[1]):
-            TotalTasks += 1
-            if todo.get('completed') is True:
-                TotalCompletedTasks += 1
-                TaskDescription.append(todo.get('title'))
-    print('Employee {} is done with tasks({}/{}):'.format(Employee,
-                                                          TotalCompletedTasks,
-                                                          TotalTasks))
-
-    for task in TaskDescription:
-        print('\t {}'.format(task))
-
-
-if __name__ == '__main__':
-    getData()
+if __name__ == "__main__":
+    import requests
+    import sys
+    num = sys.argv[1]
+    api_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    response = requests.get(api_url)
+    dicti = response.json()
+    todoTask = []
+    tasks = 0
+    for todo in dicti:
+        tasks = tasks + 1
+        if todo["completed"] is True:
+            todoTask.append(todo["title"])
+    api_url = "https://jsonplaceholder.typicode.com/users/{}".format(num)
+    response = requests.get(api_url)
+    name = response.json()["name"]
+    tot = len(todoTask)
+    print("Employee {} is done with tasks({}/{}):".format(name, tot, tasks))
+    for item in todoTask:
+        print("\t {}".format(item))
--- 0x15-api/test/0-gather_data_from_an_API.py
+++ 0x15-api/test/0-gather_data_from_an_API.py
@@ -7,3 +7,24 @@ import sys
 if __name__ == '__main__':
     employedId = sys.argv[1];
     baseUrl = "https://jsonplaceholder.typicode.com/users"
+
+    url = baseUrl + '/' + employedId;
+    response = requests.get(url);
+    employeeName = response.json().get('name');
+
+    todoUrl = url + '/todos';
+    response = requests.get(todoUrl);
+    tasks = response.json();
+
+    done = 0;
+    doneTask = [];
+
+    for task in tasks:
+        if task.get('completed'):
+            doneTask.append(tasks);
+            print(tasks);
+            done += 1;
+
+    #print("Employee {} is done with tasks({}/{}):".format(employeeName, done, len(tasks)));
+
+
